0a653425bd91bf78c8f2f2f50f87fbc69a6eb4d6
Harsh Karande <harshskarande@gmail.com>
2021-03-15 17:48:12 +0530

    feat: add tailwind with styled-jsx typings

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: {
+    colors: {
+      "custom-100": "#F5F5F5",
+      "custom-200": "#8A8A8A",
+      "custom-300": "#202020",
+      "custom-400": "#121212",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
