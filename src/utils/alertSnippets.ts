ad3064db90644c14e7841268c3ef9a7364c1b491
Harsh Karande <harshskarande@gmail.com>
2021-04-06 10:35:04 +0530

    feat: add alert snippets at utils

---
+++ src/utils/alertSnippets.ts
@@ -0,0 +1,19 @@
+const alertSnippets = {
+  credentials:
+    "The credentials you are using are invalid. Try again with different credentials or create a new account.",
+  demo: "Enter an image link to detect faces. Make sure that the link ends with a valid image format like .jpg, .png or .webp.",
+  duplicate:
+    "The email above is already being used. Try again with different email address or sign in with the same.",
+  email:
+    "The email address you are using is invalid or does not match with popular domain addresses.",
+  exceeded:
+    "Your limit of 20 face detections has exceeded. Create a new account to create 20 more detection requests.",
+  image:
+    "The request for detection came back with an error. Either the image is not compatible or this must be an internal issue. Try again or report an issue.",
+  password:
+    "The password is not strong enough. Try again with minimum eight characters, at least one lowercase letter, one uppercase letter and one number.",
+  required:
+    "Some of the above required fields are left empty. Complete the form to continue.",
+};
+
+export default alertSnippets;
---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { default as alertSnippets } from "./alertSnippets";
