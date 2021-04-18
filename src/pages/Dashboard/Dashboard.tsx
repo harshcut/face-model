29bf52645192e5365d98a59c104c1574e8edfc27
Harsh Karande <harshskarande@gmail.com>
2021-04-18 18:14:41 +0530

    feat: show response from clarifai on request

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,22 +1,86 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { Input, Button, Alert, Loading, Anchor } from "../../components";
-import { alertSnippets, reduxState } from "../../utils";
-import { logout } from "../../actions";
+import { alertSnippets, reduxState, clarifaiResponse } from "../../utils";
+import { login, logout } from "../../actions";
+import { getImageDetection } from "../../api";
 
 const Dashboard = () => {
   const { register, handleSubmit } = useForm();
   const user = useSelector((state: reduxState) => state.user.data);
   const dispatch = useDispatch();
   const outputImage = useRef<HTMLImageElement>(null);
-  const [badImage] = useState(false);
+  const [badImage, setBadImage] = useState(false);
   const [image, setImage] = useState("");
-  const [faceCount] = useState(0);
-  const [validity] = useState(true);
-  const [loading] = useState(false);
-  const [boundingBoxes] = useState<Array<React.ReactNode>>([]);
+  const [faceCount, setFaceCount] = useState(0);
+  const [validity, setValidity] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [boundingBoxes, setBoundingBoxes] = useState<Array<React.ReactNode>>(
+    []
+  );
+
+  const getBoundingBox = useCallback((clarifai: clarifaiResponse) => {
+    try {
+      const data = clarifai.outputs[0].data.regions;
+      let stash: Array<React.ReactNode> = [];
+      data.forEach((data, index: number) => {
+        const width = Number(outputImage.current!.width);
+        const height = Number(outputImage.current!.height);
+        stash.push(
+          <div
+            key={index}
+            className="bounding-box"
+            style={{
+              left: data.region_info.bounding_box.left_col * width,
+              top: data.region_info.bounding_box.top_row * height,
+              right: width - data.region_info.bounding_box.right_col * width,
+              bottom:
+                height - data.region_info.bounding_box.bottom_row * height,
+            }}
+          />
+        );
+      });
+      setBoundingBoxes(stash);
+      setFaceCount(clarifai.outputs[0].data.regions.length);
+    } catch {
+      setBoundingBoxes([]);
+      setFaceCount(0);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user.entries >= 21) {
+      setValidity(false);
+    }
+    if (outputImage.current && image) {
+      outputImage.current.onload = () => {
+        setBadImage(false);
+        setLoading(true);
+        setBoundingBoxes([]);
+        setFaceCount(0);
+        getImageDetection(image)
+          .then(({ response, status }) => {
+            if (status === 200) {
+              if (response.entries <= 21) {
+                dispatch(login({ ...user, entries: response.entries }));
+                getBoundingBox(response.clarifai);
+              } else {
+                setValidity(false);
+              }
+            } else {
+              setBadImage(true);
+              setLoading(false);
+              setBoundingBoxes([]);
+              setFaceCount(0);
+            }
+          })
+          .catch(() => dispatch(logout()))
+          .finally(() => setLoading(false));
+      };
+    }
+  }, [dispatch, getBoundingBox, image, user]);
 
   return (
     <div className="pt-6 px-5 pb-6 w-full sm:max-w-xl grid gap-3">
